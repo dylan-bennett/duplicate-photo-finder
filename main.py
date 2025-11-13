28e80b5d1fab70c9a049c6025fd441ff5762acd3
Dylan Bennett <dylan@hatchcoding.com>
2025-11-13 12:02:03 -0500

    The previous version was vibe-coded and I didn't feel close to the code. Decided to start over and do it by hand.

--- main.py
+++
@@ -1,6 +0,0 @@
-from PhotoDuplicateFinderApp import PhotoDuplicateFinderApp
-
-if __name__ == "__main__":
-    # Create and run the GUI application
-    app = PhotoDuplicateFinderApp()
-    app.run()
